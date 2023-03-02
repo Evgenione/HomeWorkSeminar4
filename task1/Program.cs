992fd7c10b97db3b59a19213f811cb93c7dfbe11
Evgenione <fallangel07021989@gmail.com>
2023-03-03 02:02:03 +0300

    add .gitignor, task1, task2, task3

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int Exponentiation(int numberA, int numberB)
+{
+    int result = 1;
+    for(int i=1; i <= numberB; i++)
+    {
+        result = result * numberA;
+    }
+    // int result = Math.Pow(numberA, NumberB);
+    return result;
+}
+
+Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int exponentiation = Exponentiation(numberA, numberB);
+Console.WriteLine("Ответ:  " + exponentiation);
